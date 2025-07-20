7c0dad7b8ff3dbfd72f9ab4bff80c6c713c50eaa
VAULTFIRE ARCHITECT-GHOSTKEY-316 <brettpowers1990@icloud.com>
2025-07-20 19:17:06 -0400

    Add signal engine to compute alignment

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,3 +1,4 @@
 from .yield_engine_v1 import calculate_yield, distribute_rewards, mark_yield_boost
 from .feedback_loop import track_behavior, check_thresholds
 from .sync_protocol import sync_ns3, sync_openai, sync_worldcoin
+from .signal_engine import pulse_tick, calculate_alignment_score
